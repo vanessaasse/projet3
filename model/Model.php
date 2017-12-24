eba585e180cfc46128864c8235c71d51ff577ee7
Vanessa Asse <vanessa.asse@gmail.com>
2017-12-24 13:10:17 +0100

    Création de trois fichiers pour l'architecture MVC et création du model.php

---
+++ model/Model.php
@@ -0,0 +1,38 @@
+<?php
+
+// dans cette classe, on se connecte à la BDD
+
+abstract class Model // une classe abstraite ne peut pas être instanciée, pas de création d'objet
+{
+    private $db; // attribut pour se connecter à la base de données
+
+
+
+    protected function runRequest ($sql, $params == null)
+    {
+        if($params == null)
+        {
+            $result = $this->getDb()->query($ql); // execution directe
+        }
+        else
+        {
+            $result = $this->getDb()->prepare($sql); // requête préparée
+            $result->execute($params);
+        }
+
+        return $result;
+    }
+
+
+
+//  fonction privée, uniquement attachée à cette classe
+    private function getDb()
+    {
+        if($this->db == null)
+        {
+            $this->db = new PDO('mysql:host=localhost;dbname=projet;charset=utf8','root', 'root',
+            array (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION));
+            return $this->db;
+        }
+    }
+}
