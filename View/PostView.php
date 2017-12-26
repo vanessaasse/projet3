7ad13486028fed400e8d7f9f7806e3a6d1e04829
Vanessa Asse <vanessa.asse@gmail.com>
2017-12-26 19:14:12 +0100

    Création du formulaire pour commentaire dans PostView.php

--- View/PostView.php
+++ View/PostView.php
@@ -16,4 +16,17 @@ Commentaires à <?= $post['title'] ?><br />
     <p><?= htmlspecialchars($comment['com_content']) ?></p>
 <?php endforeach; ?>
 
-<hr />
+
+<form method="post" action="index.php?action=addcomment">
+    <fieldset>
+        <legend>Ajouter un commentaire :</legend>
+        <table>
+        <tr><td><label>Nom / Pseudo : </label></td><td>
+        <input type="text" name="name" value="<?php if(isset($_POST['name'])) echo $_POST['name']?>" size="53" max="256" required ></td></tr>
+        <tr><td><label>Commentaire : </label></td><td>
+        <textarea name="newcomment" value="<?php if(isset($_POST['newcomment'])) echo $_POST['new comment']?>" cols="51" rows="5" required ></textarea></td></tr>
+            <tr><td><input type="hidden" name="id" value="<?= $post['id'] ?>"</td></tr>
+        <tr><td><input type="submit" value="Envoyez votre message" /></td></tr>
+        </table>
+    </fieldset>
+</form>
