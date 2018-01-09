5f464333477e2a28a886a68f3159f629f634dcd3
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-09 18:00:02 +0100

    Correction de la largueur des commentaires et du formulaire pour ajouter un comment. Responsive

--- view/post/post.php
+++ view/post/post.php
@@ -15,35 +15,39 @@
 
 <section class="commentaires">
     <h3 class="intro">Commentaires à propos du <?= $this->sanitize($post['title']) ?></h3>
-
     <div class="table-wrapper">
-<?php foreach ($comments as $comment): ?>
 
-    <table>
+        <?php foreach ($comments as $comment): ?>
+
+        <table>
         <tr>
             <div class="box">
-    <strong>Par <?= $this->sanitize($comment['author']) ?><br />
-        Le <?= $this->sanitize($comment['date_fr']) ?><br/></strong>
-        <p><?= $this->sanitize($comment['com_content']) ?></p>
+            <strong>Par <?= $this->sanitize($comment['author']) ?><br />
+            Le <?= $this->sanitize($comment['date_fr']) ?><br/></strong>
+            <p><?= $this->sanitize($comment['com_content']) ?></p>
             </div>
         </tr>
+        </table>
 
+        <?php endforeach; ?>
 
-    </table>
-
-<?php endforeach; ?>
     </div>
 </section>
+<hr/>
+
+<section class="addcomment">
+    <h3 class="intro">Ajoutez un commentaire :</h3>
 
 <form method="post" action="post/addComment/<?= $post['id'] ?>">
-    <fieldset>
-        <legend>Ajoutez un commentaire :</legend>
-        <table>
-        <tr><td><label>Nom / Pseudo : </label></td><td>
-        <input type="text" name="name" size="53" max="256" required ></td></tr>
-        <tr><td><label>Commentaire : </label></td><td>
-        <textarea name="comment" cols="51" rows="5" required ></textarea></td></tr>
-        <tr><td><input type="submit" value="Envoyez votre message" /></td></tr>
-        </table>
-    </fieldset>
+
+        <label>Nom / Pseudo : </label>
+        <input type="text" name="name" size="53" max="256" required >
+    <br/>
+        <label>Commentaire : </label>
+        <textarea name="comment" cols="51" rows="5" required ></textarea>
+    <br/>
+        <input type="submit" value="Envoyez votre message" />
+
 </form>
+
+</section>
