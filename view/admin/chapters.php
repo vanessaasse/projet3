bc51a75f7666942ba10d9fe81c0885fdf737e949
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-17 22:39:45 +0100

    [PHP] Création de la vue Chapters pour afficher, dans un tableau, tous les articles édités par l'auteur.

---
+++ view/admin/chapters.php
@@ -0,0 +1,37 @@
+<?php $this->title = "Billet pour l'Alaska - Administration"; ?>
+
+<div class="breadcrumb">
+    <li><a href="http://localhost:8888/projet3fw/admin/chapters"> >> Les différents chapitres</a></li>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                <thead>
+                <tr class="danger">
+                    <th>Les chapitres</th>
+                    <th>Date de publication</th>
+                    <th>Nombre de commentaires</th>
+                    </div>
+                </tr>
+
+                </thead>
+
+                <tbody>
+                <?php foreach ($posts as $post): ?>
+                <tr>
+<td><?= $this->sanitize($post['title']) ?></td>
+                    <td>Le <?= $this->sanitize($post['date_fr']) ?></td>
+    <td><?= $this->sanitize($nbCommentsChp) ?></td>
+                </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
+
+
