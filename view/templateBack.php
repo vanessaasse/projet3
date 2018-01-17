1725d3ab672f7bf56d6b19de09cb9db11d398b1a
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-17 22:36:13 +0100

    [PHP] Modification de la class "page-wrapper"

--- view/templateBack.php
+++ view/templateBack.php
@@ -87,14 +87,15 @@
 
     <!--fin du corps de texte -->
 
-    </div>
+
 
 
 <footer>
     <hr />
-    © 2017 - Vanessa Asse - <a href="https://openclassrooms.com/paths/chef-de-projet-multimedia-developpement" target="blank">
+    <a href="https://fr.linkedin.com/in/vanessa-asse-665a2030" target="blank">© 2017 - Vanessa Asse -</a> <a href="https://openclassrooms.com/paths/chef-de-projet-multimedia-developpement" target="blank">
         Projet 3 dans la cadre de la formation<br />"Chef de projet multimédia - spécialité Développement" avec Openclassrooms</a>
 </footer>
+</div>
 
 
 <!-- /. WRAPPER  -->
