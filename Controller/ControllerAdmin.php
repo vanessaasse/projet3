cc150a9d51ef498428b49d164ce9d1e8d1a289f7
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-20 09:37:23 +0100

    [PHP] Ajout de - $nbComments = $this->comment->getNumberComments() - l. 34 dans la methode Index, simplification de la methode chapters

--- Controller/ControllerAdmin.php
+++ Controller/ControllerAdmin.php
@@ -1,8 +1,8 @@
 <?php
 
 require_once 'ControllerSecure.php';
-require_once 'model/post.php';
-require_once 'model/comment.php';
+require_once 'Model/Post.php';
+require_once 'Model/Comment.php';
 
 /**
  * Controller des actions de l'espace administration du site
@@ -40,9 +40,33 @@ class ControllerAdmin extends ControllerSecure
 
     public function chapters()
     {
-        $posts = $this->post->getPosts(); // va chercher la méthode getPosts dans le model post.php l.20
-        $nbCommentsChp = $this->comment->getNumberCommentsbychapter($postId);
-        $this->buildView(array('posts' => $posts, 'nbCommentsChp'=> $nbCommentsChp));
+        $posts = $this->post->getPosts(); // va chercher la méthode getPosts dans le Model Post.php l.20
+        $this->buildView(array('posts' => $posts));
     }
+
+
+    /*
+
+    public function create()
+
+    public function edit()
+    {
+        $id = $this->request->getParameter("id");
+        //1- recuperer le post associé à l'id $post
+
+
+        //2- si j'arrive sur cettepage en POST avec title et content de soumis
+             // mettre à jour $post dans la base et redirection
+        // sinon
+            //
+
+        $title = $this->request->getParameter("title"); // name dans le formulaire
+        $content = $this->request->getParameter("content"); // content dans le formulaire
+        // Sauvegarde du commentaire
+        $this->post->editPost($title, $content); // va chercher la méthode dans le Model post l.63
+
+
+        $this->buildView(array('title' => $title, 'content'=> $content));
+    } */
 
 }
