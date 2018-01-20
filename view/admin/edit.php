8d477672996ecc7cb534a18f193766229f361d2e
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-20 20:43:22 +0100

    [PHP] Création de la vue edit.

--- view/admin/edit.php
+++ view/admin/edit.php
@@ -5,13 +5,18 @@
 </div>
 
 
-<form method="post" action="">
+<form method="post" action="admin/edit/<?= $_POST['id'] ?>">
 
+    <input type="hidden" name="id" value="$_POST['id']">
+
+    <label>Date</label>
+    <input type="date" name="date" value="$_POST['date']">
+    <br />
     <label>Titre : </label>
-    <input type="text" name="title" size="53" max="600" required value="">
+    <input type="text" name="title" size="53" max="600" required value="$_POST['title']">
     <br/>
     <label>Contenu : </label>
-    <textarea name="content" cols="51" rows="5" required ></textarea>
+    <textarea name="content" cols="51" rows="5" required value="$_POST['content']"></textarea>
     <br/>
     <input type="submit" value="Enregistrer" />
 
