29cbe102ac83ce48797022d3478ac65bb47a39ea
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-21 19:36:23 +0100

    [CSS] Intégration de la vue admin/post.php

--- view/admin/post.php
+++ view/admin/post.php
@@ -1,7 +1,30 @@
 <?php $this->title = "Billet simple pour l'Alaska"; ?>
 
+<div class="breadcrumb">
+    <li><a href="admin/post/<?= $this->sanitize($post['id']) ?>">Editer "<?= $this->sanitize($post['title']) ?>"</a><br/>Publié le <?= $this->sanitize($post['date_fr']) ?></li>
+</div>
 
-        <?= $this->sanitize($post['title']) ?><br/>
-        Publié le <?= $this->sanitize($post['date_fr']) ?><br/>
 
-        <?= $this->sanitize($post['content']) ?>
+<div class="row">
+
+    <div class="col-lg-10">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="admin-post">
+                <?= $this->sanitize($post['content']) ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-2">
+        <div class="alert alert-warning"><i class=" fa fa-refresh "></i>Mettre à jour</div>
+
+        <div class="alert alert-success"><i class="fa fa-edit "></i> Publier</div>
+
+        <div class="alert alert-danger"><i class="fa fa-pencil"></i> Supprimer</div>
+    </div>
+</div>
+
+
+
