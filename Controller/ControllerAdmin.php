e146ef06d1f9a8b421c7aeeb55d857aa3de72ec2
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-21 19:35:47 +0100

    [PHP] Modification de la ligne 69 pour corriger le message d'erreur - " Notice: Use of undefined constant create - assumed 'create' in /Users/vanessa/Documents/Openclassrooms/projet3fw/Controller/ControllerAdmin.php on line 69 - Remplacement de $this->buildView(dataview:create) par $this->buildView()

--- Controller/ControllerAdmin.php
+++ Controller/ControllerAdmin.php
@@ -66,7 +66,7 @@ class ControllerAdmin extends ControllerSecure
         }
         else
         {
-                $this->buildView(create);
+                $this->buildView();
         }
     }
 
