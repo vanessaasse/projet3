93b288b31f12119a81b0b09bff1c330796a4d9dd
Vanessa Asse <vanessa.asse@gmail.com>
2018-01-22 12:14:21 +0100

    [JS & CSS] Le fichier script de Tinymce a été copié dans la partie head du templateback et non dans cette vue.

--- view/admin/create.php
+++ view/admin/create.php
@@ -1,30 +1,18 @@
 <?php $this->title = "Billet pour l'Alaska - Administration"; ?>
 
-<head>
-    <script type="text/javascript" src="content/tinymce/tinymce.min.js"></script>
-    <script type="text/javascript">
-        tinyMCE.init({
-            mode : "textareas",
-            theme : "modern",
-            language : "fr_FR",
-        });
-    </script>
-</head>
-
-
 
 <div class="breadcrumb">
     <li><a href="admin/create"> >> Ajouter un chapitre </a></li>
 </div>
 
 
-<form name="formulaire" id="formulaire" method="post" action="admin/create">
+<form method="post" action="admin/create">
 
     <label>Titre : </label>
     <input type="text" name="title" size="53" max="600" placeholder="Saisir votre titre" required value="<?php if(isset($title)) echo $title ?>">
     <br/>
     <label>Contenu : </label>
-    <textarea name="content" cols="51" rows="20" required ><?php if(isset($content)) echo $content  ?></textarea>
+    <textarea name="content" cols="51" rows="20" ><?php if(isset($content)) echo $content  ?></textarea>
     <br/>
     <input type="submit" value="Enregistrer" />
 
