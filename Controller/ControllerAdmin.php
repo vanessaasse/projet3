7b0d83f6bcee4650e0374302d7d931bf5cbdf131
Vanessa Asse <vanessa.asse@gmail.com>
2018-02-09 13:55:06 +0100

    [PHP] L.65 à 97 : modification de la méthode create pour afficher un message d'erreur en fonction de la condition dans laquelle on arrive.

--- Controller/ControllerAdmin.php
+++ Controller/ControllerAdmin.php
@@ -38,12 +38,19 @@ class ControllerAdmin extends ControllerSecure
         $this->buildView(array('nbPosts' => $nbPosts, 'nbComments' => $nbComments, 'posts'=>$posts, 'comments' =>$comments, 'login' => $login));
     }
 
+
+    /**
+     * Méthode pour afficher tous les chapitres dans la vue Chapters
+     */
     public function chapters()
     {
         $posts = $this->post->getPosts(); // va chercher la méthode getPosts dans le Model Post.php l.20
         $this->buildView(array('posts' => $posts));
     }
 
+    /**
+     * Fonction pour afficher un post en entier
+     */
     public function post()
     {
         $postId = $this->request->getParameter("id"); /* le parametre disparait lors de l'annonce de "public function post($postId)". On l'annonce
@@ -52,45 +59,46 @@ class ControllerAdmin extends ControllerSecure
         $this->buildView(array('post' => $post));
     }
 
-
+    /**
+     * Fonction pour créer un post
+     */
     public function create()
     {
+        // j'appelle toutes les variables qui peuvent arriver par défaut dans la buildview
         $title = $this->request->getParameterByDefault('title');
         $content = $this->request->getParameterByDefault('content');
+        $errors = []; // entre crochets car c'est un array dans lequel peuvent s'afficher plusieurs messages d'erreurs.
 
-        // j'arrive en post car des données sont saisies dans le formulaire
+        // j'arrive en post, et toutes les données sont saisies dans le formulaire
         if($title && $content)
         {
             // si tous les champs sont saisis, enregistrement dans la BDD.
-            $title = $this->request->getParameter('title');
-            $content = $this->request->getParameter('content');
             $this->post->addPost($title, $content);
             $this->redirect("admin", "chapters"); // une fois le post créé, je redirige vers la vue Chapters
-    }
+        }
 
-        // j'arrive sur la vue en Get
-        else
+        // j'arrive sur la vue en POST mais le champs titre ou le champs contenu n'est pas saisi
+        // j'affiche des erreurs insérées dans un tableau.
+        else if($_SERVER['REQUEST_METHOD'] === 'POST')
         {
-            if($title && $content && is_null($title)) // si les champs titre est NULL.
+            if(!$title) // si le titre n'est pas saisi, d'où le !
             {
-                $this->buildView(array($errorMsg ?? 'errorMsg' ?? 'Le champs titre doit être saisi.'));
-                var_dump('titre');
+                $errors[] = "Le champs Titre n'a pas été saisi.";
             }
 
-            elseif($title && $content && is_null($content)) //si les champs contenu est NULL
+            if(!$content) // si le contenu n'est pas saisi, d'où le !
             {
-                $this->buildView(array($errorMsg ?? 'errorMsg'?? 'Le champs contenu doit être saisi.'));
-                var_dump('contenu');
-            }
-
-            else
-            {
-            $this->buildView(array('title' => $title, 'content' => $content));
-            var_dump('vue simple');
+                $errors[] = "Le champs Contenu n'a pas été saisi.";
             }
         }
+
+        // La buildview affiche tous les paramètres affichés par défaut l.68 à 71.
+        $this->buildView(array('title' => $title, 'content' => $content, 'errors' => $errors));
     }
 
+    /**
+     * Fonction pour modifier un post dans le BDD
+     */
     public function edit()
     {
         $id =  $this->request->getParameter('id'); // récupérer le paramètre de l'ID
@@ -112,6 +120,9 @@ class ControllerAdmin extends ControllerSecure
         $this->buildView(array('post'=>$post));
     }
 
+    /**
+     * Fonction pour supprimer un post de la BDD
+     */
     public function delete()
     {
         $post['id'] = $this->request->getParameter('id'); // récupérer le paramètre de l'ID
@@ -119,12 +130,30 @@ class ControllerAdmin extends ControllerSecure
         $this->redirect("admin", "chapters"); // une fois le post supprimé, je redirige vers la vue chapters
     }
 
-   //Affiche tous les commentaires dans l'admin
+   //Affiche tous les commentaires dans l'admin, dans la vue admin/comments
     public function Comments()
     {
-        $comments = $this->comment->getAllComments(); // va chercher la méthode getPosts dans le Model Post.php l.20
+        $comments = $this->comment->getAllComments();
         $this->buildView(array('comments' => $comments));
     }
+
+
+
+    /*public function profil()
+    {
+        $users['id'] = $this->request->getParameter('id');
+        $user = $this->user->User($id);
+
+        if ($this->request->parameterExist("newpassword")) {
+            $newpassword = $this->request->getParameter("newpassword");
+
+            }
+            else
+            {
+                $this->buildView(array('user' => $user));
+            }
+
+    }*/
 
 
 
