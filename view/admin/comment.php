b71cc623eee5d8df4343ed52a86da732541ef333
Vanessa Asse <vanessa.asse@gmail.com>
2018-02-11 17:31:27 +0100

    [PHP & CSS] Création de la vue admin/comment + mise en page

---
+++ view/admin/comment.php
@@ -0,0 +1,20 @@
+<?php $this->title = "Billet simple pour l'Alaska"; ?>
+
+<div class="breadcrumb">
+    <li><a href="admin/comment/<?= $comment['id'] ?>"> >> Modération de commentaire</a></li>
+</div>
+
+    <div class="panel panel-default">
+    <div class="panel-body">
+
+        <div class="comment">Posté par <?= $comment['author']?> le <?= $comment['date_fr']?></div><br/>
+        <?= $comment['com_content'] ?>
+        <br/><br/>
+        <div class="signal"><i class="fa fa-arrow-right"></i> Signalé <?= $comment['nb_report'] ?> fois</div>
+        <br/>
+        Approuver le commentaire<br/>
+        Supprimer le commentaire
+
+
+    </div>
+    </div>
