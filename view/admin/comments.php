b20227d4a75e06c329053c0e7d053f95c9547a16
Vanessa Asse <vanessa.asse@gmail.com>
2018-02-11 17:30:57 +0100

    [PHP] Suppression de $post['title'] pour afficher le titre du post en question

--- view/admin/comments.php
+++ view/admin/comments.php
@@ -11,10 +11,18 @@
     <div class="panel panel-default">
     <div class="panel-body">
 
-    <div class="comment">Posté par <?= $comment['author']?> le <?= $comment['date_fr']?><br/>
-        Dans "<?= $comment['posttitle']?>" </div><br/>
-    <?= $comment['com_content']?><br/>
+    <div class="comment">Posté par <?= $comment['author']?> le <?= $comment['date_fr']?></div><br/>
+        <?= substr($comment['com_content'], 0, 250) ?>
+        ... <a href="admin/comment/<?= $comment['id'] ?>">Voir le commentaire</a>
 
+        <br/><br />
+
+
+        <div class="signal">
+            <?php if($comment['nb_report'] > 0): ?>
+                <i class="fa fa-arrow-right"></i> Signalé <?= $comment['nb_report'] ?> fois<br/><br/>
+            <?php endif; ?>
+        </div>
 
     </div>
     </div>
